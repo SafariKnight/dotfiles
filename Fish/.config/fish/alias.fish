cffd5f5990d1dbd024905bcb93c82d3571a1c51b
Kareem Hisham <safariknight2@gmail.com>
2024-06-22 20:26:37 +0300

    lots of linux shit happened

--- Fish/.config/fish/alias.fish
+++ Fish/.config/fish/alias.fish
@@ -2,5 +2,5 @@ alias vim="nvim"
 alias cls="clear"
 alias v="fd -H --type f --print0 --exclude .git | fzf-tmux -p -w 85  --read0 --print0 --preview \"bat --color=always --style=numbers {}\" | xargs -r -0 nvim"
 alias ls='eza --group-directories-first'
-
 alias ovim='NVIM_APPNAME=ovim nvim'
+alias rm='trash-put'
