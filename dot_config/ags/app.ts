083c410071b05e4fcf63bb91397f6e158bf30c7f
Kareem Hisham <safariknight2@gmail.com>
2025-04-02 13:41:59 +0200

    feat: add ags/astal bar

---
+++ dot_config/ags/app.ts
@@ -0,0 +1,14 @@
+import { App } from "astal/gtk3";
+import style from "./styles/style.scss";
+import Bar from "./widget/Bar/Bar";
+import Power from "./widget/Power/Power";
+
+App.start({
+  icons: "./icons",
+  css: style,
+  main() {
+    App.get_monitors().map(Bar);
+    Power();
+  },
+  instanceName: "desktop-shell",
+});
