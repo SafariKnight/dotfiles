ac3c30ac121bdd5711d817ca691e426d85549f92
Kareem Hisham <safariknight2@gmail.com>
2025-09-14 22:30:33 +0300

    fix: wifi adapter udev rule

--- hosts/krypton/hardware-configuration.nix
+++ hosts/krypton/hardware-configuration.nix
@@ -5,6 +5,7 @@
   config,
   lib,
   modulesPath,
+  pkgs,
   ...
 }: {
   imports = [
@@ -21,6 +22,12 @@
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
+
+  services.udev.extraRules = ''
+    # Switch RTL8821CU from storage/CD-ROM mode to Wi-Fi mode
+    ATTR{idVendor}=="0bda", ATTR{idProduct}=="1a2b", \
+      RUN+="${pkgs.usb-modeswitch}/bin/usb_modeswitch -v 0bda -p 1a2b -P c811 -KW"
+  '';
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
